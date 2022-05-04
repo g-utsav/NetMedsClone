2c5bac3b2f48869521cf7070cfb0407edd5b68fe
Utsav Gupta <gutsav9@gmail.com>
2022-05-04 20:02:25 +0530

    data Structure

--- components/data.js
+++ components/data.js
@@ -1,13 +1,39 @@
-{
-    bannerImg : "https://www.netmeds.com/images/cms/aw_rbslider/slides/1649267860_Category_Banners_web_1000px_x_200px.jpg",
-    catagories : 
+let data = {
+    bannerImg : "https://www.netmeds.com/images/cms/aw_rbslider/slides/1649356712_WEB_Mom__baby-1000X200.png",
+    mastCata : 
     [
         {
             catagoryImg : "https://........",
-            name : "Name of the products"
-            disc : "if present only Number Else false"
-            data :{ 
+            name : "Name of the products" ,
+            disc : "if present only Number Else false" ,
+            catagories :
+            { 
                 subBanner : "", 
+                data : 
+                [
+                    {
+                        image : "https://.....",
+                        mkt : "xyz",
+                        price : "only Number",
+                        strike : "only Number"
+                    },
+                    {
+                        image : "https://.....",
+                        mkt : "xyz",
+                        price : "only Number",
+                        strike : "only Number"
+                    }
+                ]
+            }
+        },
+        {
+            catagoryImg : "https://........",
+            name : "Name of the products" ,
+            disc : "if present only Number Else false" ,
+            catagories :
+            { 
+                subBanner : "", 
+                data : 
                 [
                     {
                         image : "https://.....",

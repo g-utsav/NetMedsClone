7afe49b6353d7d10134b14e4bdc7ddb943ff9e11
Utsav Gupta <gutsav9@gmail.com>
2022-05-04 19:27:47 +0530

    data Structure

---
+++ components/data.js
@@ -0,0 +1,47 @@
+{
+    bannerImg : "https://.....",
+    catagories : 
+    [
+        {
+            catagoryImg : "https://........",
+            name : "Name of the products"
+            disc : "if present only Number Else false"
+            data : 
+            [
+                {
+                    image : "https://.....",
+                    mkt : "xyz",
+                    price : "only Number",
+                    strike : "only Number"
+                },
+                {
+                    image : "https://.....",
+                    mkt : "xyz",
+                    price : "only Number",
+                    strike : "only Number"
+                }
+            ]
+        }
+
+        {
+            catagoryImg : "https://........",
+            name : "Name of the products"
+            disc : "if present only Number Else false"
+            data : 
+            [
+                {
+                    image : "https://.....",
+                    mkt : "xyz",
+                    price : "only Number",
+                    strike : "only Number"
+                },
+                {
+                    image : "https://.....",
+                    mkt : "xyz",
+                    price : "only Number",
+                    strike : "only Number"
+                }
+            ]
+        }
+    ]
+}

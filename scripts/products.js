118cd2612f6c4e66619ce52e2a36925742251b99
Utsav Gupta <gutsav9@gmail.com>
2022-05-07 13:51:10 +0530

    sideBar Integrated

--- scripts/products.js
+++ scripts/products.js
@@ -6,7 +6,9 @@ function create(id){
 }
 
 import {covidEssentials, personalCare, MakeUp} from "../components/data.js"
+import {sidebar} from "../components/export.js"
 
+get("#sidebar").innerHTML = sidebar()
 let dataFuncArr = [covidEssentials,personalCare,MakeUp];
 let loadData = localStorage.getItem("loadData") || ""
 let data;
